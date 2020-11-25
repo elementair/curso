da8268cb23d0da0da363deb933bac2eaab35454f
elementair <element.earias20@gmail.com>
2020-11-25 14:24:36 -0600

    v1 folders

---
+++ ecommerce/frontend/vistas/plantilla.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="title" content="Tienda Virtual">
+	<meta name="description" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos quam vel, amet veritatis exercitationem iste ea nisi, obcaecati molestiae facilis.">
+	<meta name="keyword" content="Lorem, ipsum, dolor, sit, amet, consectetur, adipisicing, elit">
+
+	<title>Tienda Virtual</title>	
+
+	<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=fa-step-forwardp" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="vistas/css/plantilla.css">
+
+	<script src="vistas/js/plugins/jquery.min.js"></script>
+
+	<script src="vistas/js/plugins/bootsrap.min.js"></script>
+
+</head>
+<body>
+	<?php 
+		// CABEZOTE
+		include "modulos/cabezote.php"
+	 ?>
+
+	
+</body>
+</html>
